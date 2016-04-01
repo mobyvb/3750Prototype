67ddeeabf62221813048f0376722a06db091805a
Maximillian von Briesen <mobyvb@gmail.com>
2016-03-31 21:44:49 -0400

    finish some stuff

---
+++ validspots.js
@@ -0,0 +1,18 @@
+var validSpots = new Array(10);
+for (var i=0; i<10; i++) {
+  validSpots[i] = new Array(10);
+}
+
+validSpots[0][0] = true;
+validSpots[1][0] = true;
+validSpots[2][0] = true;
+validSpots[2][1] = true;
+validSpots[2][2] = true;
+validSpots[2][3] = true;
+validSpots[2][4] = true;
+validSpots[3][3] = true;
+validSpots[3][4] = true;
+validSpots[4][4] = true;
+validSpots[5][3] = true;
+validSpots[5][4] = true;
+validSpots[6][4] = true;
